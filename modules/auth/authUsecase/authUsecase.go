2600a588d63cf704f280df22975da4c6fdf05661
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-01 04:29:25 +0700

    init: intialize gRPC and HTTP handlers for authentication
    Added new gRPC and HTTP handler implementations for the authentication
    module. Introduced `authGrpcHandler` and `authHttpHandler` structs,
    along with their respective constructors. Enhanced the `authRepository`
    with a method for database connection retrieval. This update lays the
    groundwork for improved authentication service interactions.

--- modules/auth/authUsecase/authUsecase.go
+++ modules/auth/authUsecase/authUsecase.go
@@ -1,1 +1,14 @@
 package authUsecase
+
+import "github.com/Supakornn/mmorpg-shop/modules/auth/authRepository"
+
+type (
+	AuthUsecaseService interface{}
+	authUsecase        struct {
+		authRepository authRepository.AuthRepositoryService
+	}
+)
+
+func NewAuthUsecase(authRepository authRepository.AuthRepositoryService) AuthUsecaseService {
+	return &authUsecase{authRepository}
+}
