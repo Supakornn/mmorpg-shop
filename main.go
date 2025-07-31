0cbe90ec21eb50f3f4f02ba55a57dc6d7f60e4fb
Supakorn Ieamgomol <78318848+Supakornn@users.noreply.github.com>
2025-08-01 01:17:43 +0700

    Revert "Setup config (#1)" (#2)
    This reverts commit 6146a5ec5321da70333c889d316cd0784ce161fc.

--- main.go
+++ main.go
@@ -1,26 +1,3 @@
 package main
 
-import (
-	"context"
-	"log"
-	"os"
-
-	"github.com/Supakornn/mmorpg-shop/config"
-)
-
-func main() {
-	ctx := context.Background()
-	_ = ctx
-
-	// Initialize Config
-	cfg := config.LoadConfig(func() string {
-		if len(os.Args) < 2 {
-			log.Fatal("Error: Please provide a path to the .env file")
-		}
-
-		return os.Args[1]
-	}())
-
-	log.Println(cfg)
-
-}
+func main() {}
