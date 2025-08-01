c6cf510ba4437c823fa547b89a468b906e89488c
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-01 17:44:18 +0700

    init: add payment handlers and repository structure
    Introduced new HTTP handler for the payment module, including
    `paymentHttpHandler` with its constructor. Added a new repository
    structure with `paymentRepository` and its database connection method.
    This update establishes a foundational structure for payment-related
    functionalities.

--- modules/payment/paymentHandler/paymentHttpHandler.go
+++ modules/payment/paymentHandler/paymentHttpHandler.go
@@ -1,1 +1,20 @@
 package paymentHandler
+
+import (
+	"github.com/Supakornn/mmorpg-shop/config"
+	"github.com/Supakornn/mmorpg-shop/modules/payment/paymentUsecase"
+)
+
+type (
+	PaymentHttpHandlerService interface{}
+
+	paymentHttpHandler struct {
+		cfg *config.Config
+
+		paymentUsecase paymentUsecase.PaymentUsecaseService
+	}
+)
+
+func NewPaymentHttpHandler(cfg *config.Config, paymentUsecase paymentUsecase.PaymentUsecaseService) PaymentHttpHandlerService {
+	return &paymentHttpHandler{cfg, paymentUsecase}
+}
--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,1 +1,15 @@
 package paymentUsecase
+
+import "github.com/Supakornn/mmorpg-shop/modules/payment/paymentRepository"
+
+type (
+	PaymentUsecaseService interface{}
+
+	paymentUsecase struct {
+		paymentRepository paymentRepository.PaymentRepositoryService
+	}
+)
+
+func NewPaymentUsecase(paymentRepository paymentRepository.PaymentRepositoryService) PaymentUsecaseService {
+	return &paymentUsecase{paymentRepository}
+}
