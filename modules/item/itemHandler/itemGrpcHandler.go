104993d389708ffafe2bcc6b022e701258486fb4
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-04 04:49:37 +0700

    feat: implement gRPC service for item management with FindItemsInIds method
    Added a new gRPC service `ItemGrpcService` with the method
    `FindItemsInIds`, along with corresponding request and response
    structures defined in Protobuf. This enhancement improves the
    organization and functionality of item management within the
    application.

--- modules/item/itemHandler/itemGrpcHandler.go
+++ modules/item/itemHandler/itemGrpcHandler.go
@@ -1,13 +1,25 @@
 package itemHandler
 
-import "github.com/Supakornn/mmorpg-shop/modules/item/itemUsecase"
+import (
+	"context"
+
+	itemPb "github.com/Supakornn/mmorpg-shop/modules/item/itemPb"
+	"github.com/Supakornn/mmorpg-shop/modules/item/itemUsecase"
+)
 
 type (
 	itemGrpcHandler struct {
+		itemPb.UnimplementedItemGrpcServiceServer
 		itemUsecase itemUsecase.ItemUsecaseService
 	}
 )
 
 func NewItemGrpcHandler(itemUsecase itemUsecase.ItemUsecaseService) *itemGrpcHandler {
-	return &itemGrpcHandler{itemUsecase}
+	return &itemGrpcHandler{
+		itemUsecase: itemUsecase,
+	}
+}
+
+func (g *itemGrpcHandler) FindItemsInIds(ctx context.Context, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error) {
+	return nil, nil
 }
