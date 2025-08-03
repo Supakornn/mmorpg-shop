f2cfbf02433e01789b522b7d3e850062c71c13bc
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-04 01:38:38 +0700

    feat: add item entity and model structures for item management
    Introduced new types in the `item` module, including `Item`,
    `CreateItemReq`, `ItemShowCase`, `ItemSearchReq`, `ItemUpdateReq`, and
    `EnableorDisableItemReq` to manage item data effectively. This update
    enhances the organization and functionality of item management within
    the application.

--- modules/item/itemModel.go
+++ modules/item/itemModel.go
@@ -1,1 +1,36 @@
 package item
+
+import "github.com/Supakornn/mmorpg-shop/modules/models"
+
+type (
+	CreateItemReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required"`
+	}
+
+	ItemShowCase struct {
+		ItemId   string  `json:"item_id"`
+		Title    string  `json:"title"`
+		Price    float64 `json:"price"`
+		ImageUrl string  `json:"image_url"`
+		Damage   int     `json:"damage"`
+	}
+
+	ItemSearchReq struct {
+		Title string `json:"title"`
+		models.PaginateReq
+	}
+
+	ItemUpdateReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required"`
+	}
+
+	EnableorDisableItemReq struct {
+		UsageStatus bool `json:"usage_status"`
+	}
+)
