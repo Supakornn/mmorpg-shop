f653f742b0eef4b67282afb3cd1c5dd458bff0d1
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-04 02:09:15 +0700

    feat: add inventory entity and model structures for inventory management
    Introduced new types in the `inventory` module, including `Inventory`,
    `UpdateInventoryReq`, `ItemInventory`, and `PlayerInventory` to manage
    inventory data effectively. This update enhances the organization and
    functionality of inventory management within the application.

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/Supakornn/mmorpg-shop/modules/item"
+	"github.com/Supakornn/mmorpg-shop/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerId string `json:"player_id" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerId string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
