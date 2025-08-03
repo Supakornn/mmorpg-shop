986b67266ef12d55f2f3336d85cb5af443614ab8
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-03 23:26:27 +0700

    feat: add credential and role models for authentication
    Introduced new types `Credential` and `Role` in the `auth` module to
    manage user credentials and roles. Updated `authModel.go` with request
    structures for player login and token refresh, along with a profile
    interceptor. Enhanced player model with a new `PlayerProfile` type for
    user details. This update improves the structure and handling of
    authentication-related data.

--- modules/auth/authModel.go
+++ modules/auth/authModel.go
@@ -1,1 +1,34 @@
 package auth
+
+import (
+	"time"
+)
+
+type (
+	PlayerLoginReq struct {
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=32" `
+	}
+
+	RefreshTokenReq struct {
+		RefreshToken string `json:"refresh_token" form:"refresh_token" validate:"required,max=500"`
+	}
+
+	InsertPlayerRole struct {
+		PlayerId string `json:"player_id" validate:"required"`
+		RoleCode []int  `json:"role_code" validate:"required"`
+	}
+
+	ProfileIntercepter struct {
+		Credential *Credential `json:"credential"`
+	}
+
+	CredentialRes struct {
+		Id          string    `json:"_id" bson:"_id,omitempty"`
+		PlayerId    string    `json:"player_id" bson:"player_id"`
+		RoleCode    int       `json:"role_code" bson:"role_code"`
+		AccessToken string    `json:"access_token" bson:"access_token"`
+		CreatedAt   time.Time `json:"created_at"`
+		UpdatedAt   time.Time `json:"updated_at"`
+	}
+)
