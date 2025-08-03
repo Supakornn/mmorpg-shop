d6e0d910ab2876cfb85cb499b49bc86f5fc38e17
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-04 04:41:26 +0700

    feat: implement gRPC service for player management with profile and account operations
    Added a new gRPC service `PlayerGrpcService` with methods for
    `CredentialSearch`, `FindOnePlayerProfileToRefresh`, and
    `GetPlayerSavingAccount`. Introduced corresponding request and response
    structures in the protobuf definitions. This enhancement improves the
    organization and functionality of player management within the
    application.

--- modules/player/playerHandler/playerGrpcHandler.go
+++ modules/player/playerHandler/playerGrpcHandler.go
@@ -1,13 +1,33 @@
 package playerHandler
 
-import "github.com/Supakornn/mmorpg-shop/modules/player/playerUsecase"
+import (
+	"context"
+
+	playerPb "github.com/Supakornn/mmorpg-shop/modules/player/playerPb"
+	"github.com/Supakornn/mmorpg-shop/modules/player/playerUsecase"
+)
 
 type (
 	playerGrpcHandler struct {
+		playerPb.UnimplementedPlayerGrpcServiceServer
 		playerUsecase playerUsecase.PlayerUsecaseService
 	}
 )
 
 func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *playerGrpcHandler {
-	return &playerGrpcHandler{playerUsecase}
+	return &playerGrpcHandler{
+		playerUsecase: playerUsecase,
+	}
+}
+
+func (g *playerGrpcHandler) CredentialSearch(ctx context.Context, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) FindOnePlayerProfileToRefresh(ctx context.Context, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) GetPlayerSavingAccount(ctx context.Context, req *playerPb.GetPlayerSavingAccountReq) (*playerPb.GetPlayerSavingAccountRes, error) {
+	return nil, nil
 }
