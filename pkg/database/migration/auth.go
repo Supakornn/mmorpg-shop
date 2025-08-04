38aeb519f7de174c6ebce19c290e0fd82070a4b7
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-04 17:58:08 +0700

    feat: implement authentication database migration and initialization
    Added a new migration package for handling authentication database
    operations, including connection setup and index creation for the `auth`
    and `roles` collections. Implemented role data insertion during
    migration. Updated the main migration script to support authentication
    migration based on application configuration.

---
+++ pkg/database/migration/auth.go
@@ -0,0 +1,73 @@
+package migration
+
+import (
+	"context"
+	"log"
+
+	"github.com/Supakornn/mmorpg-shop/config"
+	"github.com/Supakornn/mmorpg-shop/modules/auth"
+	"github.com/Supakornn/mmorpg-shop/pkg/database"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func AuthDbConn(pctx context.Context, cfg *config.Config) *mongo.Database {
+	return database.DbConn(pctx, cfg).Database("auth_db")
+}
+
+func AuthMigrate(pctx context.Context, cfg *config.Config) {
+	db := AuthDbConn(pctx, cfg)
+	defer db.Client().Disconnect(pctx)
+
+	col := db.Collection("auth")
+
+	// Indexs
+	// Auth
+	indexs, _ := col.Indexes().CreateMany(pctx, []mongo.IndexModel{
+		{Keys: bson.D{{Key: "_id", Value: 1}}},
+		{Keys: bson.D{{Key: "player_id", Value: 1}}},
+		{Keys: bson.D{{Key: "refresh_token", Value: 1}}},
+	})
+	for _, index := range indexs {
+		log.Printf("Index: %s created", index)
+	}
+
+	// Roles
+	col = db.Collection("roles")
+
+	indexs, _ = col.Indexes().CreateMany(pctx, []mongo.IndexModel{
+		{Keys: bson.D{{Key: "_id", Value: 1}}},
+		{Keys: bson.D{{Key: "code", Value: 1}}},
+	})
+	for _, index := range indexs {
+		log.Printf("Index: %s created", index)
+	}
+
+	// Role Datas
+	documents := func() []any {
+		roles := []*auth.Role{
+			{
+				Title: "user",
+				Code:  0,
+			},
+			{
+				Title: "admin",
+				Code:  1,
+			},
+		}
+
+		docs := make([]any, 0)
+		for _, r := range roles {
+			docs = append(docs, r)
+		}
+
+		return docs
+	}()
+
+	results, err := col.InsertMany(pctx, documents)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Migrate auth completed", results)
+}
--- pkg/database/script/migration.go
+++ pkg/database/script/migration.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Supakornn/mmorpg-shop/config"
+	"github.com/Supakornn/mmorpg-shop/pkg/database/migration"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize Config
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: Please provide a path to the .env file")
+		}
+
+		return os.Args[1]
+	}())
+
+	switch cfg.App.Name {
+	case "auth":
+		migration.AuthMigrate(ctx, &cfg)
+	case "player":
+	case "item":
+	case "inventory":
+	}
+}
